c751638c7a9f2b61821ec38ae608f909345a76e4
cloud <yc359032@gmail.com>
2024-05-04 21:42:16 +0800

    update

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from .xray import xray
 from .car import car
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(xray, prefix="/api/xray", tags=["xray"])
 app.include_router(car, prefix="/api/car", tags=["car"])
